2fabfcd5ed60925642eb7d3dd6417f97e608a834
kazuki morokado <>
2019-06-08 21:58:39 +0900

    fix scheme

--- askfp/db/schema.rb
+++ askfp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_07_102346) do
+ActiveRecord::Schema.define(version: 2019_06_08_120214) do
 
   create_table "reservations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "fp_id"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2019_06_07_102346) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_type", default: "guest"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
