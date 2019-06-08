7507f0ace5de9698fead42be582636970800416e
kazuki morokado <>
2019-06-08 17:59:22 +0900

    assosiation追加:User,Reservation validation追加

--- askfp/app/models/reservation.rb
+++ askfp/app/models/reservation.rb
@@ -1,2 +1,9 @@
 class Reservation < ApplicationRecord
+  belongs_to :fp, class_name: 'User', foreign_key: 'fp_id'
+  belongs_to :guest, class_name: 'User', foreign_key: 'guest_id'
+
+  validates :fp_id, presence: true
+  validates :guest_id, presence: true
+  validates :start_at, presence: true
+  validates :reservation_date, presence: true
 end
