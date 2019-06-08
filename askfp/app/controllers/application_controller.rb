3ce34d4944a71f8ed2909b9387cb12d7271a8f38
kazuki morokado <>
2019-06-08 21:31:53 +0900

    FP/ゲストユーザで新規登録出来るように修正

--- askfp/app/controllers/application_controller.rb
+++ askfp/app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   include ReservationsHelper
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_type])
+  end
 end
