f37c84c7b7793f8bf4977fda54bfa9b13a684bea
kazuki morokado <>
2019-06-08 21:57:41 +0900

    user scope追加

--- askfp/app/models/user.rb
+++ askfp/app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :reservation, dependent: :destroy
+
+  scope :fp_user, -> { where(user_type: "fp") }
+  scope :guest_user, -> { where(user_type: "guest") }
+
+  def is_fp?
+    self.user_type == "fp"
+  end
+
+  def is_guest?
+    self.user_type == "guest"
+  end
 end
