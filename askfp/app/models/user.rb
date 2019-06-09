af166e2c295b7c7bff333be829de966ed94d71a5
kazuki morokado <>
2019-06-09 18:22:25 +0900

    assosiation追加:user,profile

--- askfp/app/models/user.rb
+++ askfp/app/models/user.rb
@@ -4,9 +4,12 @@ class User < ApplicationRecord
 
   has_many :reservations, foreign_key: "fp_id" , dependent: :destroy
   has_many :reservations, foreign_key: "guest_id" , dependent: :destroy
+  has_one :profile, dependent: :destroy
 
   scope :fp_user, -> { where(user_type: "fp") }
   scope :guest_user, -> { where(user_type: "guest") }
+
+  after_create :create_profile
 
   def is_fp?
     self.user_type == "fp"
@@ -14,5 +17,9 @@ class User < ApplicationRecord
 
   def is_guest?
     self.user_type == "guest"
+  end
+
+  def create_profile
+    Profile.create(user_id: self.id)
   end
 end
