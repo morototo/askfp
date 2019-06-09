4763463706f9dab32cf7bbea1420e8ba10e5661a
kazuki morokado <>
2019-06-09 18:21:52 +0900

    profile migration追加

--- askfp/db/migrate/20190609085222_create_profiles.rb
+++ askfp/db/migrate/20190609085222_create_profiles.rb
@@ -1,6 +1,9 @@
 class CreateProfiles < ActiveRecord::Migration[5.2]
   def change
     create_table :profiles do |t|
+      t.references :user, index: true, null: false
+      t.string :name, default: ''
+      t.text :self_introduction
 
       t.timestamps
     end
