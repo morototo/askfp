27b5146e9da5974e983e4ddc3c078301d422e951
kazuki morokado <>
2019-06-14 13:59:50 +0900

    rspec: DashboardController テスト追加

--- askfp/spec/controllers/dashboard_controller_spec.rb
+++ askfp/spec/controllers/dashboard_controller_spec.rb
@@ -8,11 +8,18 @@ RSpec.describe DashboardController, type: :controller do
 
   describe 'GET #index' do
     context 'ログインしている' do
+      before :each do
+        sign_in @fp_user
+      end
       it 'ダッシュボード画面にアクセス出来ること' do
+        get :index
+        expect(response).to render_template :index
       end
     end
     context 'ログインしていない' do
       it 'ダッシュボード画面にアクセス出来ないこと' do
+        get :index
+        expect(response).not_to render_template :index
       end
     end
   end
