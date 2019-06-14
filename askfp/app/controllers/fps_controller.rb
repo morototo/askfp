0af3dc3beff537e31e4e4145a16de48360061fe5
kazuki morokado <>
2019-06-14 18:09:42 +0900

    FP一覧画面にはFPはアクセス出来ないように修正

--- askfp/app/controllers/fps_controller.rb
+++ askfp/app/controllers/fps_controller.rb
@@ -1,9 +1,15 @@
 class FpsController < ApplicationController
   before_action :authenticate_user!
+  before_action :check_user_type, only: [:search]
   def show
   end
 
   def search
     @fp_user = User.fp_user
+  end
+
+  private
+  def check_user_type
+    redirect_to root_path if current_user.is_fp?
   end
 end
