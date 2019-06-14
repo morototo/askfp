df35a0ae4a7f82c0d72f4cde59132fa7f5be4ace
kazuki morokado <>
2019-06-14 18:16:35 +0900

    before_actionをapplication_controllerに移動

--- askfp/app/controllers/fps_controller.rb
+++ askfp/app/controllers/fps_controller.rb
@@ -1,15 +1,10 @@
 class FpsController < ApplicationController
   before_action :authenticate_user!
-  before_action :check_user_type, only: [:search]
+  before_action :check_is_guest, only: [:search]
   def show
   end
 
   def search
     @fp_user = User.fp_user
-  end
-
-  private
-  def check_user_type
-    redirect_to root_path if current_user.is_fp?
   end
 end
