505d70d6bb22db09ab6a5959dd9eddd403898451
kazuki morokado <>
2019-06-14 13:59:01 +0900

    rspec: factory FpNgTimeFrame 修正

--- askfp/spec/factories/fp_ng_time_frame.rb
+++ askfp/spec/factories/fp_ng_time_frame.rb
@@ -3,19 +3,19 @@ FactoryBot.define do
     association :user, factory: :user
 
     trait :set_weekday_time_frame do
-      # transient do
-      #   time_frame_id 1
-      # end
-      time_frame_id { 1 }
+      ignore do
+        time_frame_id 1
+      end
+      time_frame_id { time_frame_id }
       is_weekday { true }
       is_holiday { false }
     end
 
     trait :set_holiday_time_frame do
-      # transient do
-      #   time_frame 1
-      # end
-      time_frame_id { 1 }
+      ignore do
+        time_frame_id 1
+      end
+      time_frame_id { time_frame_id }
       is_weekday { false }
       is_holiday { true }
     end
