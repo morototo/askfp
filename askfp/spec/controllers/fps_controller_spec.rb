7a3e58a53f616368150abf092408e32379aae23f
kazuki morokado <>
2019-06-14 18:10:00 +0900

    rspec: FpsController テスト追加

--- askfp/spec/controllers/fps_controller_spec.rb
+++ askfp/spec/controllers/fps_controller_spec.rb
@@ -7,12 +7,28 @@ RSpec.describe FpsController, type: :controller do
   end
 
   describe 'GET #search' do
-    context 'ログインしている' do
-      it 'FP一覧画面にアクセス出来ること' do
+    context 'ログインしている：ゲスト' do
+      before :each do
+        sign_in @guest_user
+      end
+      it 'ゲスト：FP一覧画面にアクセス出来ること' do
+        get :search
+        expect(response).to render_template :search
+      end
+    end
+    context 'ログインしている：FP' do
+      before :each do
+        sign_in @fp_user
+      end
+      it 'FP：FP一覧画面にアクセス出来ない' do
+        get :search
+        expect(response).not_to render_template :search
       end
     end
     context 'ログインしていない' do
-      it 'FP一覧画面にアクセス出来ること' do
+      it 'FP一覧画面にアクセス出来ないこと' do
+        get :search
+        expect(response).not_to render_template :search
       end
     end
   end
