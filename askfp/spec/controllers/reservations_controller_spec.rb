dbd6e2333d8e5d64f3e2a1af3df3ee3364e9ba37
kazuki morokado <>
2019-06-15 11:01:38 +0900

    rspec: ReservationController テスト追加

--- askfp/spec/controllers/reservations_controller_spec.rb
+++ askfp/spec/controllers/reservations_controller_spec.rb
@@ -6,30 +6,57 @@ RSpec.describe ReservationsController, type: :controller do
     @guest_user = FactoryBot.create(:user, :guest)
   end
 
-  describe 'GET #show' do
-    context 'ログインしている' do
+  describe 'GET #new' do
+    context 'ログインしている: ゲスト' do
+      before :each do
+        sign_in @guest_user
+      end
       it '予約一覧画面が表示できる' do
+        get :new
+        expect(response).to render_template :new
+      end
+    end
+    context 'ログインしている: FP' do
+      before :each do
+        sign_in @fp_user
+      end
+      it '予約一覧画面が表示できない' do
+        get :new
+        expect(response).not_to render_template :new
       end
     end
     context 'ログインしていない' do
       it '予約一覧画面が表示できない' do
+        get :new
+        expect(response).not_to render_template :new
       end
     end
   end
 
   describe 'GET #index' do
     context 'ログインしている' do
+      before :each do
+        sign_in @guest_user
+      end
       it '予約画面が表示できる' do
+        get :index
+        expect(response).to render_template :index
       end
     end
     context 'ログインしていない' do
       it '予約画面が表示できない' do
+        get :index
+        expect(response).not_to render_template :index
       end
     end
   end
 
   describe 'POST #create' do
+    let!(:valid_create_params) { { reservation: FactoryBot.attributes_for(:reservation, fp_id: , guest_id: @guest_user.id, reservation_date: start_at: "12:00" ) } }
     context 'ログインしている' do
+      before :each do
+        sign_in @guest_user
+      end
       it 'ゲスト: 予約が保存される' do
       end
       it 'FP: 予約が保存されない' do
