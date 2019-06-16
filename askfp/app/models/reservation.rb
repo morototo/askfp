a88f0d6801541e3623f838b3cfedc364bdffb662
kazuki morokado <>
2019-06-16 20:16:01 +0900

    予約時のvalidation追加

--- askfp/app/models/reservation.rb
+++ askfp/app/models/reservation.rb
@@ -7,6 +7,8 @@ class Reservation < ApplicationRecord
   validates :start_at, presence: true
   validates :reservation_date, presence: true
 
+  validate :reservation_time_check
+
   scope :reserved, -> fp_id { where(fp_id: fp_id) }
   scope :target_date, -> target_date { where(reservation_date: target_date) }
 
@@ -25,8 +27,48 @@ class Reservation < ApplicationRecord
   end
 
   def set_end_at
-    frame_min  = (60 * 30)
+    frame_min  = (60 * 30) # 30分
     self.end_at = (Time.parse(self.start_at) + frame_min).strftime("%H:%M")
     save!
+  end
+
+  def reservation_time_check
+    errors.add(:reservation_date, "既に予約されています。") and return if already_reserved?
+    # errors.add(:reservation_date, "この時間帯は予約ができません") and return if fp_ng_time?
+    errors.add(:reservation_date, "日曜日は予約できません。") and return if self.reservation_date.sunday?
+    if self.reservation_date.saturday?
+      errors.add(:start_at, "時間外の予約はできません。土曜日の予約時間帯は11:00 ~ 15:00です。") unless between_saturday_time?
+    else
+      errors.add(:start_at, "時間外の予約はできません。平日の予約時間帯は10:00 ~ 18:00です。") unless between_weekday_time?
+    end
+  end
+
+  def already_reserved?
+    Reservation.where(reservation_date: self.reservation_date.strftime("%Y-%m-%d"), start_at: self.start_at).count > 1
+  end
+
+  # def fp_ng_time?
+  #   time_frame_id = Timeframe.find_by(start_at: self.start_at).pluck(:id)
+  #   FpNgTimeFrame.where(user_id: self.user_id, time_frame_id: time_frame_id) > 0
+  # end
+
+  def between_saturday_time?
+    saturday_day_start_hour = 11
+    saturday_day_end_hour   = 14
+    start_time_min_0    = 0
+    start_time_min_30   = 30
+    start_time = Time.parse(self.start_at)
+    saturday_day_start_hour <= start_time.hour.to_i && start_time.hour.to_i <= saturday_day_end_hour && 
+      (start_time.min == start_time_min_0 || start_time.min == start_time_min_30)
+  end
+
+  def between_weekday_time?
+    week_day_start_hour = 10
+    week_day_end_hour   = 17
+    start_time_min_0    = 0
+    start_time_min_30   = 30
+    start_time = Time.parse(self.start_at)
+    (week_day_start_hour <= start_time.hour.to_i && start_time.hour.to_i <= week_day_end_hour) && 
+      (start_time.min == start_time_min_0 || start_time.min == start_time_min_30)
   end
 end
