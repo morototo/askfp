5af46c8ed3bcbef6af1b116b6de752bfeed38d69
kazuki morokado <>
2019-06-17 21:34:05 +0900

    不要なroute削除

--- askfp/config/routes.rb
+++ askfp/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :fp_ng_time_frames, only: [:index, :create]
   resources :profiles, only: [:edit, :update]
   resources :dashboard, only: [:index]
-  resources :reservations do
+  resources :reservations, only: [:show, :new, :create] do
     collection do
       get 'set_time'
     end
